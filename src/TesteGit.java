e4bb5a40901c0ae5021899cacdc8cdddb052b8c4
guilherme ribeiro <guilherme.ribeiro@supero.com.br>
2014-11-04 11:41:59 -0200

    tirando .iml do git

---
+++ src/TesteGit.java
@@ -0,0 +1,19 @@
+import org.joda.time.DateTime;
+
+public class TesteGit{
+
+
+	
+	public static void main(String args[]){
+
+
+        DateTime date = new DateTime();
+
+
+        System.out.println(date);
+
+    }
+
+
+
+}
